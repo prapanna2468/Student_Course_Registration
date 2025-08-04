3b3df3a4fb4017419356ed26669e2fe121128dbe
prapanna2468 <161803920+prapanna2468@users.noreply.github.com>
2025-08-04 20:59:10 +0545

    Update Main.java

--- src/main/java/com/student/registration/Main.java
+++ src/main/java/com/student/registration/Main.java
@@ -2,23 +2,25 @@ package com.student.registration;
 
 import com.student.registration.utils.SceneManager;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        // Initialize the SceneManager with the primary stage
         SceneManager.getInstance().setPrimaryStage(primaryStage);
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         primaryStage.setTitle("Student Course Registration System");
-        primaryStage.setResizable(false);
-        primaryStage.setWidth(1000);
-        primaryStage.setHeight(700);
-        
-        // Load login scene
-        SceneManager.getInstance().switchScene("login.fxml");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMaximized(true); // Start maximized instead of fullscreen for better UX
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
